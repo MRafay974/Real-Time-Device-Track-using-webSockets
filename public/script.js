1b143cd550ffa38235ede1a1548057d66fac6ee1
Muhammad Rafay Munir <107582975+MRafay974@users.noreply.github.com>
2025-01-25 00:58:53 +0500

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,45 @@
+const socket = io()
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position)=>{
+      const  {latitude,longitude}=position.coords
+      socket.emit("user-location",{latitude,longitude})
+    }, (error)=>{
+        console.log(error)
+    },{
+        enableHighAccuracy:true,
+        timeout:5000,
+        maximumAge:0
+    }
+)
+}
+
+
+const map=L.map("map").setView([0,0],15)
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+,{ attribution:"Rafay"}
+).addTo(map) 
+
+
+const markers={}
+
+socket.on("live-location", (data)=>{
+    const{id,latitude,longitude}=data
+    map.setView([latitude,longitude],25)
+    
+if (markers[id]) {
+    markers[id].setLatLng([latitude,longitude])
+}else{
+    markers[id]=L.marker([latitude,longitude]).addTo(map)
+}
+})
+
+socket.on("user-disconnected", (id)=>{
+    if(markers[id]){
+        map.removeLayer(markers[id])
+        delete markers[id]
+    }
+    console.log("Disconnected")
+})
+
